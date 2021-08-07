29c698e69fd133c321cfce23753ed7b0a579fe0e
Daniil <RLegar@mail.ru>
2021-08-07 16:48:25 +0300

    Second practic

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -10,17 +10,31 @@ const personalMovieDB = {
     privat: false
 };
 
-const firstFilmName = prompt('Один из последних просмотренных фильмов?', ''),
-firstFilmMark = prompt('На сколько оцените его?', ''),
-secondFilmName = prompt('Одни из последних просмотренных фильмов?', ''),
-secondFilmMark = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[firstFilmName] = firstFilmMark;
-personalMovieDB.movies[secondFilmName] = secondFilmMark;
+for(let i = 0; i < 2; i++){
+    let filmName = prompt('Один из последних просмотренных фильмов?', '');
+    let filmMark = prompt('На сколько оцените его?', '');
+
+    if(filmName == '' || filmMark == '' || filmName.length > 50){
+        i -= 1;
+        continue;
+    }
+
+    personalMovieDB.movies[filmName] = filmMark;
+
+}
+
+if(personalMovieDB.count < 10){
+    alert('Вы посмотрели довольно мало фильмов');
+}
+else if(personalMovieDB > 10 && personalMovieDB < 30){
+    alert('Вы классический зритель');
+}
+else if(personalMovieDB.count > 30){
+    alert('Вы киноман!');
+}
+else {
+    alert('Произошла ошибка!');
+}
 
 console.log(personalMovieDB);
-
-
-
-
-
